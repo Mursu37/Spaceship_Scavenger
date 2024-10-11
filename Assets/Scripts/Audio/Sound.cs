afd1df38e5ace2e6d760c85a9be8db0a08cc752a
Tappajahauki <46774318+Tappajahauki@users.noreply.github.com>
2024-10-11 11:52:57 +0300

    Updated AudioManager

--- Assets/Scripts/Audio/Sound.cs
+++ Assets/Scripts/Audio/Sound.cs
@@ -7,6 +7,15 @@ public class Sound
     public string name;
     public AudioClip clip;
     public AudioMixerGroup mixerGroup;
+
+    [Range(0f, 1f)] public float spatialBlend = 0f; // 0 = 2D, 1 = 3D
+    public float minDistance = 1f;
+    public float maxDistance = 500f;
+    public AudioRolloffMode rolloffMode = AudioRolloffMode.Logarithmic;
+
+    public bool bypassReverbZones = false;
+    public bool bypassEffects = false;
+
     [HideInInspector]
     public AudioSource source;
 }
