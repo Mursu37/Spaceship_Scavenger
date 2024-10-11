5591926376b390fca93b01f9018df032af54ecfe
Arina Vasileva <107575324+arinavasileva@users.noreply.github.com>
2024-10-11 13:54:13 +0300

    Door Opening Scene Added

---
+++ Assets/DoorOpening.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpening : MonoBehaviour
+{
+    private Animator _doorAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _doorAnimator = GetComponent<Animator>();
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _doorAnimator.SetTrigger("Open");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _doorAnimator.SetTrigger("Closed");
+        }
+   
+    }
+}
