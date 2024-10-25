379701f46b0b84e2a6cff38cc699e629475e9b3b
ShadySeal <leevi.lehtonen@proton.me>
2024-10-25 20:48:43 +0300

    Fixed few issues that occurred during the building process

--- Assets/Scripts/CLI/OpenDoor.cs
+++ Assets/Scripts/CLI/OpenDoor.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
+//using UnityEditor.Animations;
 using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
