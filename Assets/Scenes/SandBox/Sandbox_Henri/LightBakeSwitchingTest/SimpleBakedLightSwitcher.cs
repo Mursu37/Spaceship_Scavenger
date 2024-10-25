61766fa5698b3a712ba9dde6cace4e714be60523
Henriiko <ikonen.4rt@gmail.com>
2024-10-25 16:24:38 +0300

    LightMap Switching Test Success

--- Assets/Scenes/SandBox/Sandbox_Henri/LightBakeSwitchingTest/SimpleBakedLightSwitcher.cs
+++ Assets/Scenes/SandBox/Sandbox_Henri/LightBakeSwitchingTest/SimpleBakedLightSwitcher.cs
@@ -1,41 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.Rendering;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.InputSystem;
 
 public class SimpleBakedLightSwitcher : MonoBehaviour
 {
 
+    public GameObject[] darkLightPrefab, brightLightPrefab;
+
     public Texture2D[] darkLightmapDir, darkLightmapColor;
     public Texture2D[] brightLightmapDir, brightLightmapColor;
     private LightmapData[] darkLightmap, brightLightmap;
+    public ReflectionProbe[] lightReflectionProbe, darkReflectionProbe;
 
     // Start is called before the first frame update
     void Start()
     {
         List<LightmapData> dLightmap = new List<LightmapData>();
-       
-       for(int i = 0; i < darkLightmapDir.Length; i++)
-       {
+
+        for (int i = 0; i < darkLightmapDir.Length; i++)
+        {
             LightmapData lmdata = new LightmapData();
-            //lmdata.lightmapDir = darkLightmapDir[i];
+            lmdata.lightmapDir = darkLightmapDir[i];
             lmdata.lightmapColor = darkLightmapColor[i];
 
             dLightmap.Add(lmdata);
-       }
+        }
 
-       darkLightmap = dLightmap.ToArray();
+        darkLightmap = dLightmap.ToArray();
 
         List<LightmapData> blightmap = new List<LightmapData>();
 
-         for(int i = 0; i < brightLightmapDir.Length; i++)
-       {
+        for (int i = 0; i < brightLightmapDir.Length; i++)
+        {
             LightmapData lmdata = new LightmapData();
-            //lmdata.lightmapDir = brightLightmapDir[i];
+            lmdata.lightmapDir = brightLightmapDir[i];
             lmdata.lightmapColor = brightLightmapColor[i];
 
-            dLightmap.Add(lmdata);
-       }
+            blightmap.Add(lmdata);
+        }
 
         brightLightmap = blightmap.ToArray();
 
@@ -55,17 +60,40 @@ public class SimpleBakedLightSwitcher : MonoBehaviour
          }
     }
 
+    private void EnablePrefabCollection(GameObject[] gameObjectList)
+    {
+        foreach (GameObject _go in gameObjectList)
+        {
+            _go.SetActive(true);
+        }
+    }
+
+    private void DisablePrefabCollection(GameObject[] gameObjectList)
+    {
+        foreach (GameObject _go in gameObjectList)
+        {
+            _go.SetActive(false);
+        }
+    }
+
     private void OnLight1Switched()
     {
         print("Light1Switched");
-       // LightSensor[0].enabled = !LightSensor[0].enabled;
-       LightmapSettings.lightmaps = darkLightmap;
+        // LightSensor[0].enabled = !LightSensor[0].enabled;
+        DisablePrefabCollection(brightLightPrefab);
+        EnablePrefabCollection(darkLightPrefab);
+        LightmapSettings.lightmaps = darkLightmap;
+
+
     }
 
     private void OnLight2Switched()
     {
         print("Light2Switched");
-       // LightSensor[1].enabled = !LightSensor[1].enabled;
+        // LightSensor[1].enabled = !LightSensor[1].enabled;
+        DisablePrefabCollection(darkLightPrefab);
+        EnablePrefabCollection(brightLightPrefab);
         LightmapSettings.lightmaps = brightLightmap;
+ 
     }
 }
