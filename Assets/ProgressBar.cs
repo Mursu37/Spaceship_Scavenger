6314ca4a5fab5712c675b1293ec8142081b084e0
Arina Vasileva <107575324+arinavasileva@users.noreply.github.com>
2024-11-01 13:00:31 +0200

    Updated Stage text and Progress Bar Timer

--- Assets/ProgressBar.cs
+++ Assets/ProgressBar.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class PrograssBar : MonoBehaviour
+public class ProgressBar : MonoBehaviour
 {
     public GameObject bar;
-    public int time = 10;
+    public int time = 900; // 15 minutes in seconds
 
     private Vector3 initialScale;
+    private Vector3 initialPosition;
 
     private void Start()
     {
         initialScale = bar.transform.localScale;
+        initialPosition = bar.transform.localPosition;
         StartCoroutine(RunTimer());
     }
 
@@ -24,10 +25,16 @@ public class PrograssBar : MonoBehaviour
         {
             float progress = 1 - (elapsedTime / time);
             bar.transform.localScale = new Vector3(initialScale.x * progress, initialScale.y, initialScale.z);
+            bar.transform.localPosition = new Vector3(
+                initialPosition.x - (initialScale.x - bar.transform.localScale.x) / 2,
+                initialPosition.y,
+                initialPosition.z);
+
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
         bar.transform.localScale = new Vector3(0, initialScale.y, initialScale.z);
+        bar.transform.localPosition = initialPosition - new Vector3(initialScale.x / 2, 0, 0);
     }
 }
