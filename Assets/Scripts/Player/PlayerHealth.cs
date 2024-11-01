0dce337f69d5942551af791a3d9056520502995b
Arina Vasileva <107575324+arinavasileva@users.noreply.github.com>
2024-11-01 13:49:38 +0200

    Update the HUD stages

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -3,21 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour, IHealth
 {
     public float currentHealth;
     [SerializeField] private float maxHealth = 5f;
+    [SerializeField] private GameObject healthImage50; // for UI
+    [SerializeField] private GameObject healthImage25;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+        UpdateHealthUI(); // UI
     }
 
     public void Damage(float amount)
     {
         currentHealth -= amount;
         Camera.main.GetComponent<CameraShake>().shakeDuration = 0.2f;
+        UpdateHealthUI();
     }
 
     private void Update()
@@ -27,6 +32,26 @@ public class PlayerHealth : MonoBehaviour, IHealth
         {
             Scene scene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(scene.name);
+        }
+    }
+
+    private void UpdateHealthUI()
+    {
+        // Showing the appropriate health UI images based on the current health
+        if (currentHealth <= maxHealth * 0.25f)
+        {
+            healthImage25.SetActive(true);
+            healthImage50.SetActive(false);
+        }
+        else if (currentHealth <= maxHealth * 0.50f)
+        {
+            healthImage25.SetActive(false);
+            healthImage50.SetActive(true);
+        }
+        else
+        {
+            healthImage25.SetActive(false);
+            healthImage50.SetActive(false);
         }
     }
 }
