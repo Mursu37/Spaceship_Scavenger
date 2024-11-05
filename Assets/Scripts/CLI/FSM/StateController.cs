18cc1bfd23605207138ffaa819f933635f69b461
Mursu <mursujesse@gmail.com>
2024-11-05 12:49:04 +0200

    updated CLI fixed xray missing material

---
+++ Assets/Scripts/CLI/FSM/StateController.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace CLI.FSM
+{
+    public class StateController : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text commandLineText;
+        [SerializeField] private TMP_InputField commandLineInput;
+        [SerializeField] private TMP_Text directoryText;
+        
+        private State currentState;
+        private List<State> stateHistory;
+        private State defaultState;
+        private string path;
+
+        public StateController()
+        {
+            defaultState = new MainDriveState(this);
+            stateHistory = new List<State> { defaultState };
+            ChangeState(defaultState);
+        }
+
+        public void ChangeState(State newState)
+        {
+            currentState?.OnExit();
+            currentState = newState;
+            currentState.OnEnter();
+        }
+
+        public void BackOne()
+        {
+            if (stateHistory.Count <= 1)
+            {
+                ChangeText("Cannot move back from " + directoryText.text + " directory");
+                return;
+            }
+            stateHistory.Remove(stateHistory.Last());
+            ChangeState(stateHistory.Last());
+            
+            // loop through current directories to update the directoryText
+            string[] currentDirectories = directoryText.text.ToLower().Split("\\");
+            directoryText.text = "C:";
+            for (int i = 1; i < currentDirectories.Length-1; i++)
+            {
+                directoryText.text += "\\" + currentDirectories[i];
+            }
+            
+            ChangeText("Moved back to " + directoryText.text + " directory");
+        }
+
+        public void ChangeDeeper(State newState, string dirName)
+        {
+            stateHistory.Add(newState);
+            ChangeState(newState);
+            directoryText.text += "\\" + dirName;
+            ChangeText("Moved to " + directoryText.text + " directory");
+        }
+
+        public void ChangeText(string text)
+        {
+            commandLineText.text = text;
+            commandLineInput.text = "";
+            commandLineInput.ActivateInputField();
+        }
+        
+        private void OnGUI()
+        {
+            // Detects a new string of text being sent from commandLine
+            if (commandLineInput.text != "" && Input.GetKeyDown(KeyCode.Return))
+            {
+                OnInput();
+            }
+        }
+
+        private void OnInput()
+        {
+            string[] userInputs = commandLineInput.text.ToLower().Split(" ");
+            // Lets the current state to define behaviours that happen based on it's individual properties
+            currentState.Interpret(userInputs);
+        }
+    }
+}
