89de9e648c614476208a4bc11d173a35f89beeff
Arina Vasileva <107575324+arinavasileva@users.noreply.github.com>
2024-11-11 11:11:52 +0200

    Functionality for tool info ui

--- Assets/Toolinfo.cs
+++ Assets/Toolinfo.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.Rendering.HighDefinition.CameraSettings;
 
 public class Toolinfo : MonoBehaviour
 {
     private ModeSwitch modeSwitch;
     private RawImage modeImage;
+    private Cutting cutting;
     [SerializeField] private GameObject multiTool;
     [SerializeField] private Texture grapplingInfo;
-    [SerializeField] private Texture cuttingInfo;
+    [SerializeField] private Texture verticalcuttingInfo;
+    [SerializeField] private Texture horizontalcuttingInfo;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,21 +21,32 @@ public class Toolinfo : MonoBehaviour
         if (multiTool != null)
         {
             modeSwitch = multiTool.GetComponent<ModeSwitch>();
+            cutting = multiTool.GetComponent<Cutting>();
         }
 
         modeImage = GetComponent<RawImage>();
     }
 
     // Update is called once per frame
+  
     void Update()
     {
         if (modeSwitch.selectedMode == 0)
         {
             modeImage.texture = grapplingInfo;
+            //modeImage.texture = grapplingInfo;
         }
         else if (modeSwitch.selectedMode == 1)
         {
-           modeImage.texture = cuttingInfo;
+            if (!cutting.isVerticalCut)
+            {
+                modeImage.texture = horizontalcuttingInfo;
+            }
+            else
+            {
+                modeImage.texture = verticalcuttingInfo;
+            }
+            //modeImage.texture = cuttingInfo;
         }
     }
 }
