c0788f33a4446ba938f66c4e94e639e1625d31b5
Henriiko <ikonen.4rt@gmail.com>
2024-11-18 16:57:33 +0200

    Made Placeholder for Hackable UI functions

--- Assets/Scripts/UI/ListNavigationScript.cs
+++ Assets/Scripts/UI/ListNavigationScript.cs
@@ -12,16 +12,86 @@ public class ListNavigationScript : MonoBehaviour
     [SerializeField]
     private float navigationCooldown = 0.2f;
     private float cooldownTimer = 0f;
+    [SerializeField]
+    private VerticalLayoutGroup verticalLayoutGroup;
+    private List<Selectable> selectablesList = new List<Selectable>();
+    private Selectable lastListSelectable;
+
     // Start is called before the first frame update
     void Start()
     {
         inputField = this.GetComponent<TMP_InputField>();
         _eventsystem = FindFirstObjectByType<EventSystem>();
+        AddListItems();
+    }
+
+    private void AddListItems()
+    {
+        selectablesList.AddRange(verticalLayoutGroup.gameObject.GetComponentsInChildren<Selectable>());
+        SetSelectableOrder();
+    }
+
+    private void ClearListItems()
+    {
+        selectablesList.Clear();
+    }
+
+    private void SetSelectableOrder()
+    {
+        Debug.Log("Number of selectables: " + selectablesList.Count);
+
+        if (selectablesList != null && selectablesList.Count > 0)
+        {
+            for (int i = 0; i < selectablesList.Count; i++)
+            {
+                Navigation navigation = selectablesList[i].navigation;
+                navigation.mode = Navigation.Mode.Explicit;
+                navigation.selectOnUp = null;
+                navigation.selectOnDown = null;
+
+
+
+                if (i == 0)
+                {
+                    navigation.selectOnDown = selectablesList[i + 1];
+                }
+                else if (i > 0 && i < selectablesList.Count - 1)
+                {
+                    navigation.selectOnUp = selectablesList[i - 1];
+                    if (selectablesList[i + 1].interactable)
+                    {
+                        navigation.selectOnDown = selectablesList[i + 1];
+                    }
+                    else {
+                        navigation.selectOnDown = inputField as Selectable;
+                    }
+
+                }
+                else if (i == selectablesList.Count - 1)
+                {
+                    navigation.selectOnUp = selectablesList[i - 1];
+                    lastListSelectable = selectablesList[i];
+                    navigation.selectOnDown = inputField as Selectable;
+                }
+
+                selectablesList[i].navigation = navigation;
+                
+            }
+
+            Navigation inputNavigation = inputField.navigation;
+            inputNavigation.mode = Navigation.Mode.Explicit;
+            inputNavigation.selectOnUp = selectablesList[selectablesList.Count - 1];
+            inputNavigation.selectOnDown = null;
+            inputField.navigation = inputNavigation;
+
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (cooldownTimer > 0)
         {
             cooldownTimer -= Time.unscaledDeltaTime;
@@ -29,38 +99,23 @@ public class ListNavigationScript : MonoBehaviour
 
         if (inputField.isFocused)
         {
-            GameObject currentselected = EventSystem.current.currentSelectedGameObject;
-            if (currentselected == inputField.gameObject)
-            {
-                Selectable current = currentselected.GetComponent<Selectable>();
-                Selectable next = current.FindSelectableOnDown();
-                Selectable previous = current.FindSelectableOnUp();
 
                 if (cooldownTimer <= 0)
                 {
 
-
-
-                    if (Input.GetAxisRaw("Vertical") > 0.1 && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+                    if (Input.GetKeyDown(KeyCode.UpArrow))
                     {
-                        if (previous != null)
+                        if (EventSystem.current.currentSelectedGameObject == inputField.gameObject)
                         {
-                            previous.Select();
+                            Debug.Log(lastListSelectable.gameObject.name);
+                            EventSystem.current.SetSelectedGameObject(lastListSelectable.gameObject);
                             cooldownTimer = navigationCooldown; // Set cooldown
                         }
 
-                    }
-                    else if (Input.GetAxisRaw("Vertical") < -0.1 && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
-                    {
-                        if (next != null)
-                        {
-                            next.Select();
-                            cooldownTimer = navigationCooldown; // Set cooldown
-                        }
                     }
                 }
 
-            }
+
         }
     }
 }
